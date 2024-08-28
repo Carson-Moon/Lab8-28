e5f5096d0e70170281c3bce7b0cc06304c7b4c25
Carson-Moon <145728727+Carson-Moon@users.noreply.github.com>
2024-08-28 14:36:54 -0400

    Chess board Prefab

---
+++ Chess Lab/Assets/Bishop.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bishop : ChessPiece
+{
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        
+    }
+#endif
+
+    public override void CheckMoves()
+    {
+
+    }
+}
